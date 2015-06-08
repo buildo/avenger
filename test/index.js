56176192a08fc03eac9c855a05d749606fdf6ff0
Giovanni Gonzaga <giovanni@buildo.io>
2015-06-08 21:54:03 +0200

    tests with babel

--- test/index.js
+++ test/index.js
@@ -1,65 +1,12 @@
-'use strict';
+const requireDir = require('require-dir');
 
-const t = require('tcomb');
-
-const queries = require('../fixtures/queries');
-const assert = require('better-assert');
-const Query = require('../src/Query');
-
-describe('Query', () => {
-  it('should have the right structure', () => {
-    const q1 = new Query({
-      paramsType: t.struct({
-        orderId: t.Num
-      }),
-      fetchResultType: t.struct({
-        order: t.Any
-      }),
-      fetch: (params) => () => ({
-        order: null
-      })
-    });
-    const q2 = new Query({
-      paramsType: t.struct({
-        id: t.Num
-      }),
-      fetchResultType: t.struct({
-        result: t.Any
-      }),
-      dependencies: [
-        {
-          query: q1,
-          fetchParams: (q1) => ({
-            a: 'a'
-          }),
-          multi: 'p1'
-        }
-      ],
-      fetch: (params) => (q1) => ({
-        result: q1.a
-      })
-    });
-    assert(Query.is(q1));
-    assert(Query.is(q2));
-  });
+require('babel/register')({
+  ignore: /node_modules/,
+  extensions: ['.js', '.jsx'],
+  stage: 0,
+  loose: true
 });
 
-describe('Fixture', () => {
-  it('should be processed correctly', () => {
-    console.log(queries.sampleQuery);
-  });
+requireDir('./tests', {
+  recurse: true
 });
-//const sampleDetailTests = {
-//  dependencies: {
-//    sampleDetail: {
-//      params: ({ tests }) => ({
-//        testId: tests.map(({ _id }) => _id)
-//      }),
-//      multi: 'testId'
-//    }
-//  },
-//
-//  fetch: () => ({ testId }) => ({
-//    testRemarks: API.fetchTestRemark({ testId })
-//  })
-//}
---
+++ test/tests/index.js
@@ -0,0 +1,63 @@
+const t = require('tcomb');
+
+const queries = require('../../fixtures/queries');
+const assert = require('better-assert');
+const Query = require('../../src/Query');
+
+describe('Query', () => {
+  it('should have the right structure', () => {
+    const q1 = new Query({
+      paramsType: t.struct({
+        orderId: t.Num
+      }),
+      fetchResultType: t.struct({
+        order: t.Any
+      }),
+      fetch: (params) => () => ({
+        order: null
+      })
+    });
+    const q2 = new Query({
+      paramsType: t.struct({
+        id: t.Num
+      }),
+      fetchResultType: t.struct({
+        result: t.Any
+      }),
+      dependencies: [
+        {
+          query: q1,
+          fetchParams: (q1) => ({
+            a: 'a'
+          }),
+          multi: 'p1'
+        }
+      ],
+      fetch: (params) => (q1) => ({
+        result: q1.a
+      })
+    });
+    assert(Query.is(q1));
+    assert(Query.is(q2));
+  });
+});
+
+describe('Fixture', () => {
+  it('should be processed correctly', () => {
+    console.log(queries.sampleQuery);
+  });
+});
+//const sampleDetailTests = {
+//  dependencies: {
+//    sampleDetail: {
+//      params: ({ tests }) => ({
+//        testId: tests.map(({ _id }) => _id)
+//      }),
+//      multi: 'testId'
+//    }
+//  },
+//
+//  fetch: () => ({ testId }) => ({
+//    testRemarks: API.fetchTestRemark({ testId })
+//  })
+//}
