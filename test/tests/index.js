af042b7af69db2a4f888b6d417267bfdd9e647a1
Andrea Lattuada <andrea@buildo.io>
2015-06-18 13:26:19 +0200

    lint

--- test/tests/index.js
+++ test/tests/index.js
@@ -1,11 +1,7 @@
-import t from 'tcomb';
 import expect from 'expect';
 import sinon from 'sinon';
-import assign from 'lodash/object/assign';
-import zip from 'lodash/array/zip';
 
 import queries from '../../fixtures/queries';
-import m from '../../fixtures/models';
 import { AvengerInput } from '../../src';
 import { schedule } from '../../src';
 import AvengerCache from '../../src/AvengerCache';
