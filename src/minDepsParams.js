8f637c6ccabcbe4fe83eb343586940d020b85cb2
Gabriele Petronella <gabriele@buildo.io>
2015-12-30 13:30:12 +0100

    Use identity if dependency doesn't define map

--- src/minDepsParams.js
+++ src/minDepsParams.js
@@ -1,6 +1,9 @@
 export default function minDepsParams(dependencies, depsResults) {
-  return Object.keys(dependencies).reduce((ac, k) => ({
-    ...ac,
-    [k]: dependencies[k].map(depsResults[dependencies[k].query.id])
-  }), {});
+  return Object.keys(dependencies).reduce((ac, k) => {
+    const map = dependencies[k].map || (x => x);
+    return {
+      ...ac,
+      [k]: map(depsResults[dependencies[k].query.id])
+    };
+  }, {});
 }
