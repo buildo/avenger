87989aa7d9de8652790e39ae7c6a2f2fb178975f
Giovanni Gonzaga <giovanni@buildo.io>
2016-08-12 18:40:52 +0200

    invalidation wip

--- src/graph/index.js
+++ src/graph/index.js
@@ -1,6 +1,9 @@
 import t from 'tcomb';
 import findKey from 'lodash/findKey';
 import pick from 'lodash/pick';
+import uniq from 'lodash/uniq';
+import flatten from 'lodash/flatten';
+import some from 'lodash/some';
 import { apply } from '../query/apply';
 import { invalidate as _invalidate } from '../query/invalidate';
 import { ObservableCache } from '../query/ObservableCache';
@@ -81,6 +84,32 @@ export function make(input) {
       [P]: Object.assign({}, naked, dress)
     });
   }, Object.assign({}, input));
+
+  console.time(); // eslint-disable-line no-console
+  Ps.forEach(P => {
+    const master = graph[P];
+    // start with an empty downset
+    master.slaves = [];
+    Ps.forEach(PP => {
+      if (PP !== P) { // skip self
+        const f = graph[PP].fetch;
+        if (f.type === 'composition') { // if we find a composition
+          if (findP(graph, f.master) === P) { // with this node as master
+            // it means it is part of this node downset
+            master.slaves.push(PP);
+          }
+        }
+        if (f.type === 'product') { // if we find a product
+          if (some(f.fetches, ff => findP(graph, ff) === P)) { // with this node among `product.fetches`
+            // it means it is part of this node downset
+            master.slaves.push(PP);
+          }
+        }
+      }
+    });
+  });
+  console.timeEnd();  // eslint-disable-line no-console
+  return graph;
 }
 
 function queriesAndArgs(graph, Ps, A) {
@@ -98,14 +127,20 @@ export function query(graph, Ps, A) {
   return apply(queries, args);
 }
 
-export function invalidate(graph, Ps, A) {
-  const { queries, args } = queriesAndArgs(graph, Ps, A);
-  const toRefetch = Object.keys(queries).map(P => {
-    const fetch = queries[P];
-    const needsRefetch = _invalidate(fetch, args[P]);
-    return { fetch, needsRefetch, P };
-  }).filter(i => i.needsRefetch);
-  toRefetch.forEach(({ fetch, P }) => {
-    fetch(args[P]);
-  });
+function _refetchPs(graph, Ps) {
+  return Ps.reduce((ps, P) => {
+    const slaves = graph[P].slaves;
+    if (slaves.length === 0) {
+      return ps.concat(P);
+    }
+    return uniq(ps.concat(flatten(slaves.map(p => _refetchPs(graph, [p])))));
+  }, []);
+}
+
+export function invalidate(graph, invalidatePs, A) {
+  const refetchPs = _refetchPs(graph, invalidatePs);
+  const { args: invalidateArgs } = queriesAndArgs(graph, invalidatePs, A);
+  invalidatePs.forEach(P => _invalidate(graph[P].cachedFetch, invalidateArgs[P]));
+  const { args: refetchArgs } = queriesAndArgs(graph, refetchPs, A);
+  refetchPs.forEach(P => (graph[P].cachedFetch || graph[P].fetch)(refetchArgs[P]));
 }
