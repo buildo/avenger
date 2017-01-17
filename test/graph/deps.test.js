0a581814f53d5cad7f69512ecbb1639a176a4a6a
Francesco Cioria <firefelix@gmail.com>
2017-01-17 19:09:22 +0100

    add test for slave being re-fetched when observed and master changes

---
+++ test/graph/deps.test.js
@@ -0,0 +1,65 @@
+import 'rxjs'
+import assert from 'assert'
+import { make, invalidate, query } from '../../src/graph';
+import { Expire, available } from '../../src/cache/strategies';
+import Node from './Node'
+
+const state = {
+  foo: null,
+  slaveFetchCount: null
+}
+
+const MASTER_EXPIRE_TIME = 100;
+
+const makeTestGraph = () => {
+  const master = Node({
+    id: 'master',
+    strategy: new Expire(MASTER_EXPIRE_TIME),
+    params: { token: 'token' },
+    fetch: () => Promise.resolve({ bar: { foo: state.foo } })
+  });
+  const slave = Node({
+    id: 'slave',
+    strategy: available,
+    dependencies: {
+      master: { query: master }
+    },
+    fetch: ({ master: { bar } }) => {
+      state.slaveFetchCount = state.slaveFetchCount + 1;
+      return Promise.resolve(bar);
+    }
+  })
+
+  return make({ ...master, ...slave });
+}
+
+describe('graph/deps', () => {
+
+  it('"slave" should be re-fetched if "master" changes and is being observed', (done) => {
+    const graph = makeTestGraph()
+
+    state.foo = 'foo_1';
+    state.slaveFetchCount = 0;
+
+    const slave = query(graph, ['slave'], { token: 'token' })
+
+    slave.subscribe(() => {}); // add a subscriber to "slave"
+
+    // invalidate "master" after its cache has already expired
+    setTimeout(() => {
+      state.foo = 'foo_2';
+      invalidate(graph, ['master'], { token: 'token' }) // invalidate later on
+
+      // verify that "slave" has been correctly re-fetched
+      setTimeout(() => {
+        slave.subscribe((r) => {
+          assert.equal(r.data.slave.data.foo, 'foo_2');
+          assert.equal(state.slaveFetchCount, 2);
+          done();
+        });
+      }, MASTER_EXPIRE_TIME * 2)
+    }, MASTER_EXPIRE_TIME * 2);
+
+  });
+
+});
