9dfd5bfe8c5c77db577f752f14d87430e1e07f08
Giovanni Gonzaga <giovanni@buildo.io>
2017-05-11 18:23:34 +0200

    make tests fail on UnhandledPromiseRejectionWarning

--- test/jestSetup.js
+++ test/jestSetup.js
@@ -1,1 +1,8 @@
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+// In Node v7 unhandled promise rejections will terminate the process
+if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
+  process.on('unhandledRejection', reason => {
+    throw reason
+  })
+  // Avoid memory leak by adding too many listeners
+  process.env.LISTENING_TO_UNHANDLED_REJECTION = true
+}
