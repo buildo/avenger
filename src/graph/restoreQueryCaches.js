13f4e7fa3bcef8370d74344efb5fda0eb6e7c503
Giovanni Gonzaga <giovanni@buildo.io>
2017-05-30 16:52:00 +0200

    add micro debug log

--- src/graph/restoreQueryCaches.js
+++ src/graph/restoreQueryCaches.js
@@ -1,10 +1,14 @@
 import t from 'tcomb';
 import { ExtractedQueryCaches } from './extractQueryCaches';
+import debug from 'debug';
+
+const log = debug('avenger:restoreQueryCaches');
 
 export function restoreQueryCaches(graph, data) {
   t.assert(ExtractedQueryCaches.is(data));
   Object.keys(data).forEach(queryName => {
     data[queryName].forEach(({ a, value }) => {
+      log('restoring %s(%o)=%o', queryName, a, value);
       graph[queryName].fetch.cache.storePayload(a, value, Promise.resolve(value));
     });
   });
