75a7274dee9ef92b0bd63e991d0670b9e2d781e6
Francesco Cioria <firefelix@gmail.com>
2018-01-26 17:23:18 +0100

    move d.ts inline with their .js file

--- typings/strategies.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'avenger/lib/cache/strategies' {
-  export class Strategy { }
-  export class Expire extends Strategy {
-    constructor(millis: number)
-  }
-  export const available: Strategy;
-}
