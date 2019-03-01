ee8fbc0086fcf48a3415d1e820a72b9bede5d6fd
Giovanni Gonzaga <giovanni@buildo.io>
2019-03-01 15:47:00 +0100

    ground zero

--- src/index.js
+++
@@ -1,8 +0,0 @@
-export * from './cache/strategies';
-export { query, querySync } from './query';
-export { invalidate } from './invalidate';
-export { Query } from './QueryNode';
-export { Command } from './Command';
-export { runCommand } from './runCommand';
-export { extractQueryCaches } from './extractQueryCaches';
-export { restoreQueryCaches } from './restoreQueryCaches';
