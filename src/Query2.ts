db0d719b1b592bb596b69ddc060cce6ea0a03636
Giovanni Gonzaga <giovanni@buildo.io>
2019-03-05 21:47:12 +0100

    sketch Monad3 instance

--- src/Query2.ts
+++ src/Query2.ts
@@ -1,7 +1,7 @@
-import { TaskEither } from 'fp-ts/lib/TaskEither';
+import { TaskEither, taskEither } from 'fp-ts/lib/TaskEither';
 import { Either } from 'fp-ts/lib/Either';
 import { EnforceNonEmptyRecord } from './Query';
-import { Functor3 } from 'fp-ts/lib/Functor';
+import { Monad3 } from 'fp-ts/lib/Monad';
 
 export interface Fetch<A = void, L = unknown, P = unknown> {
   (input: A): TaskEither<L, P>;
@@ -34,6 +34,14 @@ export class CachedQuery<A, L, P> {
     return new CachedQuery((a: A) => this.value(a).map(f));
   }
 
+  ap<U, L, A, B>(fab: Query<U, L, (a: A) => B>): Query<U, L, B> {
+    // TODO: actually product()
+  }
+
+  chain<U, L, A, B>(f: (a: A) => Query<U, L, B>): Query<U, L, B> {
+    // TODO: actually compose()
+  }
+
   run(a: A): Promise<Either<L, P>> {
     return this.value(a).run();
   }
@@ -53,6 +61,10 @@ export class CompositionQuery<A, L, P> {
   map<B>(f: (p: P) => B): Query<A, L, B> {
     return new CompositionQuery(this.master, p1 => this.slave(p1).map(f));
   }
+
+  ap<U, L, A, B>(fab: Query<U, L, (a: A) => B>): Query<U, L, B> {}
+
+  chain<U, L, A, B>(f: (a: A) => Query<U, L, B>): Query<U, L, B> {}
 
   run(a: A): Promise<Either<L, P>> {
     return this.master(a)
@@ -77,6 +89,10 @@ export class ProductQuery<A, L, P> {
     );
   }
 
+  ap<U, L, A, B>(fab: Query<U, L, (a: A) => B>): Query<U, L, B> {}
+
+  chain<U, L, A, B>(f: (a: A) => Query<U, L, B>): Query<U, L, B> {}
+
   run(a: EnforceNonEmptyRecord<A>): Promise<Either<L, P>> {
     return this.value(a).run();
   }
@@ -86,7 +102,29 @@ function map<A, L, P, B>(fa: Query<A, L, P>, f: (p: P) => B): Query<A, L, B> {
   return fa.map(f);
 }
 
-export const query: Functor3<URI> = {
+function ap<U, L, A, B>(
+  fab: Query<U, L, (a: A) => B>,
+  fa: Query<U, L, A>
+): Query<U, L, B> {
+  return fa.ap(fab);
+}
+
+function chain<U, L, A, B>(
+  fa: Query<U, L, A>,
+  f: (a: A) => Query<U, L, B>
+): Query<U, L, B> {
+  return fa.chain(f);
+}
+
+function of<A, L, P>(p: P): Query<A, L, P> {
+  // TODO: actually cache
+  return new CachedQuery((_: A) => taskEither.of<L, P>(p));
+}
+
+export const query: Monad3<URI> = {
   URI,
-  map
+  map,
+  of,
+  ap,
+  chain
 };
