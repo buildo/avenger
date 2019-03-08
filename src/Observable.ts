435624840b0330f797899b4aab61e3e0c60298a3
Giovanni Gonzaga <giovanni@buildo.io>
2019-03-08 09:44:14 +0100

    add custom Observable fp-ts instance and rewrite observe accordingly

---
+++ src/Observable.ts
@@ -0,0 +1,59 @@
+import { Alternative1 } from 'fp-ts/lib/Alternative';
+import { Monad1 } from 'fp-ts/lib/Monad';
+import { Monoid } from 'fp-ts/lib/Monoid';
+import { combineLatest, EMPTY, merge, Observable, of as rxOf } from 'rxjs';
+import { map as rxMap, switchMap } from 'rxjs/operators';
+
+declare module 'rxjs/internal/Observable' {
+  interface Observable<T> {
+    readonly _URI: URI;
+    readonly _A: T;
+  }
+}
+
+declare module 'fp-ts/lib/HKT' {
+  interface URI2HKT<A> {
+    Observable: Observable<A>;
+  }
+}
+
+export const URI = 'Observable';
+
+export type URI = typeof URI;
+
+export const getMonoid = <A = never>(): Monoid<Observable<A>> => {
+  return {
+    concat: (x, y) => merge(x, y),
+    empty: EMPTY
+  };
+};
+
+const map = <A, B>(fa: Observable<A>, f: (a: A) => B): Observable<B> =>
+  fa.pipe(rxMap(f));
+
+const of = <A>(a: A): Observable<A> => rxOf(a);
+
+const ap = <A, B>(
+  fab: Observable<(a: A) => B>,
+  fa: Observable<A>
+): Observable<B> => combineLatest(fab, fa, (f, a) => f(a));
+
+const chain = <A, B>(
+  fa: Observable<A>,
+  f: (a: A) => Observable<B>
+): Observable<B> => fa.pipe(switchMap(f));
+
+const alt = <A>(x: Observable<A>, y: Observable<A>): Observable<A> =>
+  merge(x, y);
+
+const zero = <A>(): Observable<A> => EMPTY;
+
+export const observable: Monad1<URI> & Alternative1<URI> = {
+  URI,
+  map,
+  of,
+  ap,
+  chain,
+  zero,
+  alt
+};
--- src/observe.ts
+++ src/observe.ts
@@ -6,11 +6,11 @@ import {
   success,
   queryResult
 } from './QueryResult';
-import { of, Observable, combineLatest } from 'rxjs';
-import { distinctUntilChanged, switchMap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
+import { observable } from './Observable';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 import { ObservableQuery } from './Query';
 import { sequence, mapWithKey } from 'fp-ts/lib/Record';
-import { tuple } from 'fp-ts/lib/function';
 
 function cacheValueToQueryResult<L, P>(
   cacheValue: CacheValue<L, P>
@@ -23,10 +23,11 @@ function cacheValueToQueryResult<L, P>(
   );
 }
 
-const rxLoading = of(loading);
 const rxSkipDuplicateLoadings = distinctUntilChanged<QueryResult<any, any>>(
   (fra, frb) => fra.type === 'Loading' && frb.type === 'Loading'
 );
+const sequenceRecordObservable = sequence(observable);
+const sequenceRecordQueryResult = sequence(queryResult);
 
 export function observe<A, L, P>(
   query: ObservableQuery<A, L, P>,
@@ -34,31 +35,22 @@ export function observe<A, L, P>(
 ): Observable<QueryResult<L, P>> {
   switch (query.type) {
     case 'composition':
-      return observe(query.master, input).pipe(
-        switchMap(master =>
+      return observable
+        .chain(observe(query.master, input), master =>
           master.fold(
-            rxLoading,
-            error => of(failure(error, false)),
+            observable.of(loading),
+            error => observable.of(failure(error, false)),
             value => observe(query.slave, value)
           )
-        ),
-        rxSkipDuplicateLoadings
-      );
+        )
+        .pipe(rxSkipDuplicateLoadings);
     case 'product':
-      const observableStruct = mapWithKey(query.queries, (k, query) =>
-        // @ts-ignore
-        observe(query, input[k]).pipe(map(result => tuple(k, result)))
-      );
-      const observableTuple = Object.values(observableStruct);
-      return combineLatest(observableTuple).pipe(
-        map(trs =>
-          trs.reduce(
-            // @ts-ignore
-            (acc, tr) => ({ ...acc, [tr[0]]: tr[1] }),
-            {}
-          )
-        ),
-        map(sequence(queryResult)),
+      return sequenceRecordObservable(
+        mapWithKey(query.queries, (k, query) =>
+          observe(query, (input as any)[k])
+        )
+      ).pipe(
+        map(sequenceRecordQueryResult),
         rxSkipDuplicateLoadings
       );
     case 'cached':
