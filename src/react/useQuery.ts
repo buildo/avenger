eb06ec77d278bc6b0106fc87f62a10167b2a1426
Giovanni Gonzaga <giovanni@buildo.io>
2019-04-12 11:45:19 +0200

    WIP

---
+++ src/react/useQuery.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { ObservableQuery } from '../Query';
+import { QueryResult, loading } from '../QueryResult';
+import { observeShallow /*, read*/ } from '../observe';
+
+export function useQuery<A, L, P>(
+  query: ObservableQuery<A, L, P>,
+  input: A
+): QueryResult<L, P> {
+  const [state, setState] = useState<QueryResult<L, P>>(loading);
+
+  useEffect(() => {
+    const subscription = observeShallow(query, input).subscribe(setState);
+    return subscription.unsubscribe;
+  }, [input, query, setState]);
+
+  return state;
+}
+
+// export function useQueryOnce<A, L, P>(
+//   query: ObservableQuery<A, L, P>,
+//   input: A
+// ): QueryResult<L, P> {
+//   return read(query, input);
+// }
+
+// export function useQuerySync<A, L, P>(
+//   query: ObservableQuery<A, L, P>,
+//   input: A
+// ): QueryResult<L, P> {}
