7c947a78e2ad7e0489b71d419fb3d3f0de3e558e
Giovanni Gonzaga <giovanni@buildo.io>
2019-05-09 09:43:59 +0200

    accept a Monoid in react helpers

--- src/react/useQuery.ts
+++ src/react/useQuery.ts
@@ -1,7 +1,46 @@
 import { useEffect, useState } from 'react';
 import { ObservableQuery } from '../Query';
-import { QueryResult, loading } from '../QueryResult';
+import { QueryResult, loading, success } from '../QueryResult';
 import { observeShallow } from '../observe';
+import { Monoid } from 'fp-ts/lib/Monoid';
+import { observable } from '../Observable';
+
+export function useQueryM<A extends void, L, P>(
+  query: ObservableQuery<A, L, P>,
+  resultMonoid: Monoid<QueryResult<L, P>>,
+  input?: A
+): QueryResult<L, P>;
+export function useQueryM<A, L, P>(
+  query: ObservableQuery<A, L, P>,
+  resultMonoid: Monoid<QueryResult<L, P>>,
+  input: A
+): QueryResult<L, P>;
+export function useQueryM<A, L, P>(
+  query: ObservableQuery<A, L, P>,
+  resultMonoid: Monoid<QueryResult<L, P>>,
+  input: A
+): QueryResult<L, P> {
+  const [state, setState] = useState<QueryResult<L, P>>(resultMonoid.empty);
+
+  useEffect(() => {
+    const subscription = observable
+      .map(observeShallow(query, input), r => resultMonoid.concat(state, r))
+      .subscribe(setState);
+    return subscription.unsubscribe.bind(subscription);
+  }, [input, query, setState]);
+
+  return state;
+}
+
+function defaultMonoidResult<L, P>() {
+  return {
+    empty: loading,
+    concat: (a: QueryResult<L, P>, b: QueryResult<L, P>) =>
+      a.type === 'Success' && b.type === 'Loading'
+        ? success<L, P>(a.value, true)
+        : b
+  };
+}
 
 export function useQuery<A extends void, L, P>(
   query: ObservableQuery<A, L, P>,
@@ -15,12 +54,5 @@ export function useQuery<A, L, P>(
   query: ObservableQuery<A, L, P>,
   input: A
 ): QueryResult<L, P> {
-  const [state, setState] = useState<QueryResult<L, P>>(loading);
-
-  useEffect(() => {
-    const subscription = observeShallow(query, input).subscribe(setState);
-    return subscription.unsubscribe.bind(subscription);
-  }, [input, query, setState]);
-
-  return state;
+  return useQueryM(query, defaultMonoidResult<L, P>(), input);
 }
