b5382d116084f110491059df3015af8461a9a9d8
Giovanni Gonzaga <giovanni@buildo.io>
2019-06-06 21:52:14 +0200

    Basic jsdoc in Query.ts

--- src/Query.ts
+++ src/Query.ts
@@ -19,6 +19,9 @@ import {
   ProductP
 } from './util';
 
+/**
+ * A function that is asynchronous and can fail
+ */
 export type Fetch<A, L, P> = (input: A) => TaskEither<L, P>;
 
 interface BaseQuery<A, L, P> {
@@ -33,27 +36,44 @@ function queryPhantoms<A, L, P>(): { _A: A; _L: L; _P: P } {
   return null as any;
 }
 
+/**
+ * Represents a cached query, aka a `Fetch` that is cached with a given policy and can be `observe`d
+ */
 export interface CachedQuery<A, L, P> extends BaseQuery<A, L, P> {
   type: 'cached';
   cache: Cache<A, L, P>;
 }
 
+/**
+ * Represents a query that is the result of composing two queries that are run one after the other.
+ * The `master` is successful, its result is fed into `slave`, yielding the composition result.
+ */
 export interface Composition<A, L, P> extends BaseQuery<A, L, P> {
   type: 'composition';
   master: ObservableQuery<A, L, unknown>;
   slave: ObservableQuery<unknown, L, P>;
 }
 
+/**
+ * Represents a query that aggregates the results of N `queries` when all are successful, or yields the first failure
+ */
 export interface Product<A, L, P> extends BaseQuery<A, L, P> {
   type: 'product';
   queries: Record<string, ObservableQuery<A[keyof A], L, P[keyof P]>>;
 }
 
+/**
+ * A query that can be `observe`d
+ */
 export type ObservableQuery<A, L, P> =
   | CachedQuery<A, L, P>
   | Composition<A, L, P>
   | Product<A, L, P>;
 
+/**
+ * Constructs a `CachedQuery` given a `Fetch` function and a cache `Strategy`
+ * @param fetch The function to be cached
+ */
 export function query<A = void, L = unknown, P = unknown>(
   fetch: Fetch<A, L, P>
 ): (strategy: Strategy<A, L, P>) => CachedQuery<A, L, P> {
@@ -74,6 +94,11 @@ export type StrategyBuilder<A, L, P> = (
   cacheValueSetoid: Setoid<CacheValue<L, P>>
 ) => Strategy<A, L, P>;
 
+/**
+ * Constructs a `CachedQuery` with a `Strategy` that uses strict equality to compare inputs and results
+ * @param fetch The function to be cached
+ * @param makeStrategy Implements the `Strategy` used to filter available values from the cache
+ */
 export function queryStrict<A = void, L = unknown, P = unknown>(
   fetch: Fetch<A, L, P>,
   makeStrategy: StrategyBuilder<A, L, P>
@@ -83,6 +108,11 @@ export function queryStrict<A = void, L = unknown, P = unknown>(
   );
 }
 
+/**
+ * Constructs a `CachedQuery` with a `Strategy` that uses shallow equality to compare inputs and results
+ * @param fetch The function to be cached
+ * @param makeStrategy Implements the `Strategy` used to filter available values from the cache
+ */
 export function queryShallow<A = void, L = unknown, P = unknown>(
   fetch: Fetch<A, L, P>,
   makeStrategy: StrategyBuilder<A, L, P>
@@ -92,6 +122,11 @@ export function queryShallow<A = void, L = unknown, P = unknown>(
   );
 }
 
+/**
+ * Constructs a `CachedQuery` with a `Strategy` that uses `JSON.stringify` to compare inputs and results
+ * @param fetch The function to be cached
+ * @param makeStrategy Implements the `Strategy` used to filter available values from the cache
+ */
 export function queryJSON<A extends JSON, L extends JSON, P extends JSON>(
   fetch: Fetch<A, L, P>,
   makeStrategy: StrategyBuilder<A, L, P>
@@ -101,6 +136,11 @@ export function queryJSON<A extends JSON, L extends JSON, P extends JSON>(
   );
 }
 
+/**
+ * Constructs a `Composition`
+ * @param master A `Fetch` to run first, receiving the composition input
+ * @param slave A `Fetch` to run with the result of `master` as input
+ */
 export function compose<A1, L1, P1, L2, P2>(
   master: ObservableQuery<A1, L1, P1>,
   slave: ObservableQuery<P1, L2, P2>
@@ -123,6 +163,10 @@ export function compose<A1, L1, P1, L2, P2>(
 
 const sequenceRecordTaskEither = sequence(taskEither);
 
+/**
+ * Constructs a `Product`
+ * @param queries A record of `Fetch` functions
+ */
 export function product<R extends ObservableQueries>(
   queries: EnforceNonEmptyRecord<R>
 ): Product<ProductA<R>, ProductL<R>, ProductP<R>> {
@@ -142,6 +186,11 @@ export function product<R extends ObservableQueries>(
   };
 }
 
+/**
+ * Apply a transformation to the result of a successful `ObservableQuery`
+ * @param fa An `ObservableQuery`
+ * @param f The function transforming the success result
+ */
 export function map<U, L, A, B>(
   fa: ObservableQuery<U, L, A>,
   f: (a: A) => B
