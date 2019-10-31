342c83ef359f71d7e9c73ecf251f968aa4bfb840
Giovanni Gonzaga <giovanni@buildo.io>
2019-10-31 21:26:44 +0100

    add failing test

--- test/Query.test.ts
+++ test/Query.test.ts
@@ -1,16 +1,16 @@
-import { taskEither, fromLeft } from 'fp-ts/lib/TaskEither';
+import { taskEither } from 'fp-ts/lib/TaskEither';
 import { observe } from '../src/observe';
+import { queryStrict, queryShallow, queryJSON, map } from '../src/Query';
 import {
-  queryStrict,
-  queryShallow,
-  queryJSON,
-  map,
-  mapLeft
-} from '../src/Query';
-import { available, setoidStrict, setoidJSON, JSON } from '../src/Strategy';
+  available,
+  setoidStrict,
+  setoidJSON,
+  JSON,
+  refetch
+} from '../src/Strategy';
 import { QueryResult, getSetoid } from '../src/QueryResult';
 import { delay } from 'fp-ts/lib/Task';
-import { right, left } from 'fp-ts/lib/Either';
+import { right } from 'fp-ts/lib/Either';
 
 describe('queryStrict', () => {
   it('caches indefinitely with strategy=available', async () => {
@@ -77,15 +77,21 @@ describe('queryJSON', () => {
 });
 
 it('map', async () => {
-  const a = queryStrict(() => taskEither.of('foo'), available);
-  const b = map(a, s => s.length);
+  const f1 = jest.fn(() => taskEither.of('foo'));
+  const a = queryStrict(f1, refetch);
+  const f2 = jest.fn((s: string) => s.length);
+  const b = map(a, f2);
+  const resultSetoid = getSetoid(setoidStrict, setoidStrict);
+  const observable = observe(b, undefined, resultSetoid);
+  const observer = jest.fn();
+  observable.subscribe(observer);
   const r = await b.run().run();
   expect(r).toEqual(right(3));
-});
-
-it('mapLeft', async () => {
-  const a = queryStrict(() => fromLeft('foo'), available);
-  const b = mapLeft(a, s => s.length);
-  const r = await b.run().run();
-  expect(r).toEqual(left(3));
+  expect(f1.mock.calls.length).toBe(1);
+  expect(f2.mock.calls.length).toBe(1);
+  expect(observer.mock.calls.length).toBe(2); // pending + value
+  await a.invalidate().run();
+  expect(f1.mock.calls.length).toBe(2);
+  expect(f2.mock.calls.length).toBe(1); // not called again since map internally uses `available`
+  expect(observer.mock.calls.length).toBe(4); // (pending + value) * 2
 });
